34362b39c421638528a09336688bd9492713ba5b
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2023-10-05 09:22:57 +0200

    Filter HTML and PHP tags from input

--- zoekscherm.php
+++ zoekscherm.php
@@ -1,16 +1,11 @@
 <?php
 
-
-
-//Voordat we verder gaan: checken of er uberhaupt een $_GET is ingevuld
+Voordat we verder gaan: checken of er uberhaupt een $_GET is ingevuld
 if (empty($_GET['zoekveld'])){
-    echo "Vul wat in, faka met jou.";
+   echo "Vul wat in, faka met jou.";
 }else{
-    echo $_GET['zoekveld'];
+   echo strip_tags($_GET['zoekveld']);
 }
-
-//de zoekdata filteren
-
 //Connectie naar de database
 
 //Zoekvraag versturen naar de database
@@ -18,7 +13,7 @@ if (empty($_GET['zoekveld'])){
 
 //Controleren OF er resultaten zijn
 
-//ALS er GEEN resultaten zijn dan 'error/foutmelding'
+//ALS er GEEN re    sultaten zijn dan 'error/foutmelding'
 
 //ALS er WEL resultaten zijn dan toon resultaten
 
