702dab77f85f6d6ec41f29b257d4a0767cb60348
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2023-10-12 12:10:42 +0200

    SELECT WHERE les

--- zoekscherm.php
+++ zoekscherm.php
@@ -1,15 +1,30 @@
 <?php
 
-Voordat we verder gaan: checken of er uberhaupt een $_GET is ingevuld
 if (empty($_GET['zoekveld'])){
    echo "Vul wat in, faka met jou.";
 }else{
     $username = "root";
     $password = "";
 
-    $conn = new PDO("");
-//    echo strip_tags($_GET['zoekveld']);
+    $conn = new PDO("mysql:host=localhost;dbname=google",$username, $password);
+    // echo strip_tags($_GET['zoekveld']);
+
+    //TODO: Fix deze code zodat je met behulp van een wildcard (%) alle restaurants kan vinden met 'bere' in de naam
+    $select_restaurants = $conn->prepare("SELECT * FROM restaurants WHERE naam = :zoekveld" . "%");
+    $select_restaurants->bindParam(":zoekveld", $_GET['zoekveld']);
+    $select_restaurants->execute();
+    $restaurants = $select_restaurants->fetchAll();
+
+    // echo $restaurants[0]['naam'];
+    // echo $restaurants[0]['locatie'];
+    // echo $restaurants[1]['naam'];
+    // echo $restaurants[1]['locatie'];
+
+    foreach($restaurants as $eetzaak){
+        echo $eetzaak['naam'] . " is gevestigd in: " . $eetzaak['locatie'] . "<br>";
+    }
 }
+
 
 //Connectie naar de database
 
